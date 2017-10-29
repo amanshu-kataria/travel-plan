d93bc441b3de2ec4635dd716d98aff9bbe4bfb4e
amanshu-kataria <amanshukataria@hotmail.com>
2017-10-29 21:51:15 +0530

    Improved Left Panel

--- src/components/leftPanel.js
+++ src/components/leftPanel.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import PlanCard from "./planCard.js";
+import Stops from "./Stops.js";
 
 class LeftPanel extends Component {
   render() {
     return (
-      <PlanCard
-        name={this.props.name}
-        image={this.props.image}
-        summary={this.props.summary}
-        tags={this.props.tags}
-      />
+      <div style={{ height: 580, overflowY: "auto" }}>
+        <PlanCard
+          name={this.props.name}
+          image={this.props.image}
+          summary={this.props.summary}
+          tags={this.props.tags}
+          days={this.props.days}
+          stops={this.props.stops}
+        />
+        {this.props.stops.map((stop, index) => {
+          return (
+            <Stops
+              key={index}
+              name={stop.name}
+              image={stop.image}
+              days={stop.days}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
+// onHover={this.onHoverStop}
 
 export default LeftPanel;
