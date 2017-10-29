4e52f104b77c9ac78909dc91604ddf391a9cc137
amanshu-kataria <amanshukataria@hotmail.com>
2017-10-29 22:10:15 +0530

    Embedded Maps

--- src/components/centerPanel.js
+++ src/components/centerPanel.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
 
 class CenterPanel extends Component {
   render() {
-    return <h1>Center Panel</h1>;
+    const MyMapComponent = withScriptjs(
+      withGoogleMap(props => (
+        <GoogleMap
+          defaultZoom={8}
+          defaultCenter={{ lat: 31.51903, lng: 75.903393 }}
+        >
+          <Marker
+            position={{
+              lat: 31.51903,
+              lng: 75.903393
+            }}
+          />;
+        </GoogleMap>
+      ))
+    );
+    return (
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBa9rjRnt_NuNSUS3Wh5bMDUJVb2rZR258&libraries=places"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: "585px" }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
   }
 }
 
